26691054e9e60590dc23fe774167feb9e581a54e
Dave Ciesinski <DCiesinski@gmail.com>
2026-05-20 08:12:50 -0400

    project setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,4 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
+import { drizzle } from 'drizzle-orm/neon-http';
+const db = drizzle(process.env.DATABASE_URL!);
 
-const sql = neon(process.env.DATABASE_URL!);
-
-export const db = drizzle(sql, { schema });
-
-export type DB = typeof db;
+export { db };
